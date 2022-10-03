b30852a5528de9859fe49da9ad6a7530139ffcd6
Felix developer <felixxdeveloper@gmail.com>
2022-10-03 10:32:50 -0300

    REF - Adding makeId function at buildMakePost

--- src/domain/post/index.ts
+++ src/domain/post/index.ts
@@ -1,6 +1,7 @@
+import { randomUUID as makeId } from 'crypto';
 import { validator } from "../../infrastructure/post/validator";
 import { buildMakePost } from "./post";
 
-const makePost = buildMakePost({ validator });
+const makePost = buildMakePost({ validator, makeId });
 
 export { makePost };
