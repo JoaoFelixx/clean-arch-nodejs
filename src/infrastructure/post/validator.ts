f9a903ee922ecc20474661944d5205898874754a
Felix developer <felixxdeveloper@gmail.com>
2022-10-03 10:31:22 -0300

    REF - Adding validation at function

--- src/infrastructure/post/validator.ts
+++ src/infrastructure/post/validator.ts
@@ -3,5 +3,13 @@ import { Post } from '../../domain/entities';
 type ErrorValidator = { error?: string }
 
 export function validator(post: Post): ErrorValidator {
-  return {  }
+  const keys = Object.keys(post);
+
+  const invalidProperties = keys
+    .filter(key => !post[key])
+    .map(key => `${key} is invalid`);
+
+  return {
+    error: invalidProperties ? invalidProperties.join(', ') : undefined
+  }
 }
