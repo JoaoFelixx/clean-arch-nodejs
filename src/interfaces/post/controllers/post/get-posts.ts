eb1e1ab1d9130d71d97eafef128e84bc85bd8da4
Felix developer <felixxdeveloper@gmail.com>
2022-10-03 14:47:30 -0300

    REF - Removed unused code and added 204 status code

--- src/interfaces/post/controllers/post/get-posts.ts
+++ src/interfaces/post/controllers/post/get-posts.ts
@@ -1,9 +1,5 @@
 import { HttpRequest, HttpResponse, Post } from '../../../../domain/entities';
 
-interface ID {
-  id?: string;
-}
-
 interface FindPostMethod {
   findPosts: ({ _id }: Pick<Post, '_id'>) => Promise<Post[] | Post | Error>
 }
@@ -11,13 +7,16 @@ interface FindPostMethod {
 export const makeGetPost = ({ findPosts }: FindPostMethod) =>
   async (httpRequest: HttpRequest, httpResponse: HttpResponse) => {
     try {
-      const { id } = httpRequest.params as ID;
+      const { _id } = httpRequest.params as Pick<Post, '_id'>;
 
-      const result = await findPosts({ _id: id });
+      const result = await findPosts({ _id });
 
       if (result instanceof Error)
         return httpResponse.status(400).json(result.message);
 
+      if (!result) 
+        return httpResponse.sendStatus(204);
+        
       httpResponse.json(result)
     } catch (error) {
       return httpResponse.sendStatus(400);
