efe51a987e7f76b3b09d2202b4f5e98994b65e36
Felix developer <felixxdeveloper@gmail.com>
2022-10-03 10:38:34 -0300

    REF - Removed unused code

--- src/interfaces/post/data-access/posts-db.ts
+++ src/interfaces/post/data-access/posts-db.ts
@@ -2,11 +2,11 @@ import { Post } from '../../../domain/entities';
 import { Model } from 'mongoose';
 
 interface BuildConnection {
-  makeDb: (model: 'posts') => Model<Post>;
+  makeDb: () => Model<Post>;
 }
 
 const makePostsDb = ({ makeDb }: BuildConnection) => {
-  const db = makeDb('posts');
+  const db = makeDb();
 
   async function insert({ _id, ...post }): Promise<Post | Error> {
     try {
@@ -15,22 +15,35 @@ const makePostsDb = ({ makeDb }: BuildConnection) => {
       return result;
 
     } catch (error) {
-      console.log(error)
       return new Error('Error creating Post');
     }
   }
 
-  async function remove({ _id }: Pick<Post, '_id'>) {
+  async function get({ _id }: Partial<Pick<Post, '_id'>>): Promise<Post[] | Post | Error> {
+    try {
+      if (_id) {
+        const posts = await db.find<Post>();
+
+        return posts;
+      }
+
+      const post = await db.findOne<Post>({ _id });
+
+      return post;
+    } catch (error) {
+      return new Error('Error getting posts');
+    }
+  }
+
+  async function remove({ _id }: Pick<Post, '_id'>): Promise<void | Error> {
     try {
       await db.deleteOne({ _id });
-
-      return true;
     } catch (error) {
       return new Error('Error removing Post')
     }
   }
 
-  return Object.freeze({ insert, remove });
+  return Object.freeze({ insert, remove, get });
 };
 
 export { makePostsDb };
