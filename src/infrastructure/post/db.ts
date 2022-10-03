8b7610c7b5dea0cf6dcb102af5c7feda72c3fa1b
Felix developer <felixxdeveloper@gmail.com>
2022-10-03 10:33:26 -0300

    REF - Removing unused code

--- src/infrastructure/post/db.ts
+++ src/infrastructure/post/db.ts
@@ -4,10 +4,6 @@ import '../database/schemas/Post';
 
 const Posts = model<Post>('posts');
 
-type Models = 'posts';
-
-const makeDb = (modelSelected: Models) => ({
-  'posts': Posts,
-}[modelSelected])
+const makeDb = () => Posts;
 
 export { makeDb }
